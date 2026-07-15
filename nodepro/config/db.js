9a1a91dee85d5c0f416c8b544f8b372aa2ad8eac
Vishva2708 <bhattvishva09@gmail.com>
2026-07-15 12:36:27 +0530

    updated

--- nodepro/config/db.js
+++ nodepro/config/db.js
@@ -1,10 +1,20 @@
-const mongoose=require("mongoose")
-mongoose.connect("mongodb://localhost:27017/pro")
-const db=mongoose.connection
-db.on("connected",(data,err)=>{
-    if(err){
-        console.log(err)
-    }else{
-        console.log("database connected")
-    }
-})
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {})
+  .catch((err) => {
+    console.log("❌ Connection error:", err);
+  });
+
+const db = mongoose.connection;
+
+db.on("connected", () => {
+  console.log("✅ Database connected");
+});
+
+db.on("error", (err) => {
+  console.log("❌ Database connection error:", err);
+});
+
+module.exports = db;
